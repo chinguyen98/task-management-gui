54af83c3c41a349a1a7cd8134bb40b891c4cf7bb
chinguyen98 <dacchi6761@gmail.com>
2020-05-29 09:58:52 +0700

    login page GUI

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Container } from 'reactstrap';
-import { withRouter } from 'react-router-dom';
+import { Container, FormGroup, Form, Label, Input, Row, Col } from 'reactstrap';
+import { withRouter, NavLink } from 'react-router-dom';
 import FlashMessage from '../../components/FlashMessage';
+import { useForm } from 'react-hook-form';
 
 LoginPage.propTypes = {}
 
@@ -18,15 +19,75 @@ function LoginPage({ location, history }) {
   }
 
   const [flashMessage, setFlashMessage] = useState(redirectFlashMessage);
+  const { handleSubmit, register, errors } = useForm();
+
+  const login = async (data) => {
+    console.log(data);
+  }
 
   return (
-    <Container className='Login'>
-      <h1>Login Page</h1>
-      <FlashMessage
-        message={flashMessage.message}
-        type={flashMessage.type}
-        close={() => { setFlashMessage({}) }}
-      />
+    <Container className='registerPage mt-5'>
+      <h3>Login</h3>
+      <Row className='justify-content-center'>
+        <Col sm={10} md={7}>
+          <FlashMessage
+            message={flashMessage.message}
+            type={flashMessage.type}
+            close={() => { setFlashMessage({}) }}
+          />
+          <Form onSubmit={handleSubmit(login)}>
+            <FormGroup>
+              <Label for='username'>Username</Label>
+              <Input
+                type='text'
+                name='username'
+                id='username'
+                placeholder='Ex: nguyendacchi123'
+                className='mb-3'
+
+                innerRef={register({
+                  required: true,
+                })}
+              />
+              {
+                errors.username?.type === 'required'
+                && <div className="alert alert-danger">
+                  Username field is required!
+                  </div>
+              }
+            </FormGroup>
+            <FormGroup>
+              <Label for='password'>Password</Label>
+              <Input
+                type='password'
+                name='password'
+                id='password'
+                className='mb-3'
+
+                innerRef={register({
+                  required: true,
+                })}
+              />
+              {
+                errors.password?.type === 'required'
+                && <div className="alert alert-danger">
+                  Password field is required!
+                  </div>
+              }
+            </FormGroup>
+            <Row>
+              <Col xl={3} md={4} xs={4}>
+                <FormGroup>
+                  <Input className='btn btn-primary' type='submit' value='Login' />
+                </FormGroup>
+              </Col>
+            </Row>
+          </Form>
+          <NavLink exact to='/register'>
+            <p>Register new account</p>
+          </NavLink>
+        </Col>
+      </Row>
     </Container>
   )
 }
